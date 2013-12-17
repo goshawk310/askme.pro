f32b4f01936db0e054a42d38a7f63b140371f5e4
Adam <sojuz84@gmail.com>
2013-12-17 13:00:24 +0100

    error handler, app-development cfg

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,21 +1,36 @@
 'use strict';
-var User = require('../../models/user');
+var User = require('../../models/user'),
+  errorHandler = require('../../lib/error').handler;
 module.exports = function (server) {
-  server.param('name', /^[a-zA-Z0-9]+$/);
-  server.get('/api/user/:name', function (req, res) {
-   res.set('Content-Type', 'text/html');
-   res.send(req.param('name'));
-  });
-  server.post('/api/user/create', function (req, res) {
-    var user = new User({
-      name: req.body.name
-    });
-    user.save(function (err, user) {
+  server.param('login', /^[a-zA-Z0-9]+$/);
+  
+  server.get('/api/user/:login', function (req, res) {
+    User.findOne({login: req.param('login')}, function (err, user) {
       if (err) {
-        res.send(500, err);
-        return;
+        return errorHandler.db(err, res);
+      } else if (user === null) {
+        return res.send(404, {message: 'object not found'});
       }
       res.send(user);
+    });
+  });
+
+  server.post('/api/user/create', function (req, res) {
+    var user = new User(req.body);
+    user.save(function (err, user) {
+      if (err) {
+        return errorHandler.db(err, res);
+      }
+      res.send(user);
+    });
+  });
+
+  server.put('/api/user/update/:login', function (req, res) {
+    User.update({login: req.param('login')}, req.body, function (err, numberAffected, raw) {
+      if (err) {
+        return errorHandler.db(err, res);
+      }
+      res.send(raw);
     });
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,11 +1,21 @@
 'use strict';
-var mongoose = require('mongoose');
+
+var mongoose = require('mongoose'),
+	validate = require('mongoose-validator').validate;
+
 var User = function () {
+	
 	var schema = mongoose.Schema({
-		name: {type: String, index: {unique: true}}
+		login: {type: String, index: {unique: true}, required: true, validate: [validate('len', 4, 20), validate('isAlphanumeric')]},
+		password: {type: String, index: {unique: true}, required: true},
+		email: {type: String, required: true, valiadate: [validate('isEmail')]},
+		name: {type: String, required: true},
+		lastname: {type: String, required: true}
 	}, {
 		collection: 'user'
 	});
+
 	return mongoose.model('User', schema);
 };
+
 module.exports = new User();
