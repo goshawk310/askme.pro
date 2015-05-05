fd59f6454a60c136ff28752482248d4591ca46b7
askme <askme@n2.askme.pro>
2015-05-05 10:03:22 +0200

    fb api v2 updtes

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 
 module.exports = function(server) {
 
-    server.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'user_location']}));
+    server.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
     
     server.get('/auth/facebook/callback', 
         passport.authenticate('facebook', {
